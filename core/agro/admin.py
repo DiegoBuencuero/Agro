c7279b54ca7bf3f83659e62d51d57c93ffbbe6af
Leonardo Rodriguez <leorodriguez71@live.com>
2023-03-23 14:29:47 -0300

    temporal

--- core/agro/admin.py
+++ core/agro/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Pais, Provincia, Ciudad, Tipodoc, Nacionalidad
+from .models import Profile, Pais, Provincia, Ciudad, Tipodoc, Nacionalidad, Empresa
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Pais)
+admin.site.register(Provincia)
+admin.site.register(Ciudad)
+admin.site.register(Tipodoc)
+admin.site.register(Nacionalidad)
+admin.site.register(Empresa)
