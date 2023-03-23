dabf94ead130a71d624aab369de3029e0c4b5f35
Leonardo Rodriguez <leorodriguez71@live.com>
2023-03-23 14:05:35 -0300

    error

--- core/agro/admin.py
+++ core/agro/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-from .models import Profile, Pais, Provincia, Ciudad, Tipodoc, Nacionalidad, 
+from .models import Profile, Pais, Provincia, Ciudad, Tipodoc, Nacionalidad
 # Register your models here.
