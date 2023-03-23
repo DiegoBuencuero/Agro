b4ae41a39268a0689668d3c312acc37c3d6a6bd2
Leonardo Rodriguez <leorodriguez71@live.com>
2023-03-23 14:02:04 -0300

    Models inicio

--- core/agro/models.py
+++ core/agro/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils import timezone
 
 # Create your models here.
+class Tipodoc(models.Model):
+    def __str__(self):
+        return self.descripcion
+    descripcion = models.CharField(max_length=50)
+
+class Nacionalidad(models.Model):
+    def __str__(self):
+        return self.descripcion
+    descripcion = models.CharField(max_length=50)
+    pais = models.ForeignKey("Pais", verbose_name=("Pais"), on_delete=models.CASCADE)
+
+class Genero(models.Model):
+    def __str__(self):
+        return self.descripcion
+    descripcion = models.CharField(max_length=50)
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Provincia(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.ForeignKey("Pais", verbose_name=("Pais"), on_delete=models.CASCADE)
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+    provincia = models.ForeignKey("Provincia", verbose_name=("Provincia"), on_delete=models.CASCADE)
+
+class Empresa(models.Model):
+    class Meta:
+        pass
+    nombre = models.CharField(max_length=100)
+    razon_social = models.CharField(max_length=100)
+    cuit = models.CharField(max_length=50)
+    status = models.CharField(max_length=1, choices=[('O', 'Ok'), ('B', 'Baja'), ('S', 'Suspendido'), ], default='O')
+    add_date = models.DateTimeField(default=timezone.now)
+    
+
+
+class Profile(models.Model):
+    class Meta:
+        pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    empresa = models.ForeignKey("Empresa", on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    tipo = models.CharField(max_length=1, default='A') 
+    direccion = models.CharField(max_length=100, default='')
+    direccion2 = models.CharField(max_length=100, default='')
+    pais = models.ForeignKey("Pais", on_delete=models.CASCADE)
+    provincia = models.ForeignKey("Provincia", verbose_name=("Provincia"), on_delete=models.CASCADE)
+    ciudad = models.ForeignKey("Ciudad", verbose_name=("Ciudad"), on_delete=models.CASCADE)
+    cp = models.CharField(max_length=10, default='')
+    telefono = models.CharField(max_length=30, default='')
+    celular = models.CharField(max_length=30, default='')
+    nacionalidad = models.ForeignKey("Nacionalidad", verbose_name=("Nacionalidad"), on_delete=models.CASCADE, null=True, blank=True)
+    genero = models.ForeignKey("Genero", verbose_name=("Genero"), on_delete=models.CASCADE, null=True, blank=True)
+    tipodoc = models.ForeignKey("Tipodoc", verbose_name=("Tipo documento"), on_delete=models.CASCADE, null=True, blank=True)
+    documento = models.CharField(max_length=50, default='')
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=1, choices=[('N', 'Ingresado'), ('A', 'Aprobado'), ('S', 'Suspendido'), ], default='N')
+    add_date = models.DateTimeField(default=timezone.now)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
