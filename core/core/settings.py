0e9987b9f4119e80762928378012d4d416e14845
DiegoBuencuero <diegobuencuero4095@gmail.com>
2025-05-18 19:34:41 -0300

    bd

--- core/core/settings.py
+++ core/core/settings.py
@@ -99,15 +99,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'agro',
-       'USER': 'agro',
-       'PASSWORD': 'agro.innobrar.2023',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'agro',
+    #     'USER': 'agro',
+    #     'PASSWORD': 'agro.innobrar.2023',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
